2c82851a1c43127414794bcbf92ed35f475f5c73
Ozan Eğitmen <ozanegitmen@gmail.com>
2016-11-02 20:14:42 +0300

    Shorten build.bat

--- tools/build.bat
+++ tools/build.bat
@@ -1,9 +1,6 @@
 @echo off
 
-REM project root
-set pr=%~dp0..
-
-del /s /q "%pr%\*.pbo"
+call clear.bat
 
 cd "%pr%"
 for /d %%d in (*.*) do if /I not %%d==tools "%pr%\tools\PBOConsole" -pack "%pr%\%%d" "%pr%\%%d.pbo"
